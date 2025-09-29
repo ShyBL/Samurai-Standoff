f555af1b3cc32d907bf26d2952dee97a3201d825
Blechman Shy (NorthStar) <rakesh1985@gmail.com>
2025-09-29 15:11:55 +0300

    Background switch by difficulty

---
+++ Assets/BackgroundSwitch.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundSwitch : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> backgrounds;
+    [SerializeField] private Image background;
+    private Sprite _selectedBackground;
+
+    private void Awake()
+    {
+        _selectedBackground = LevelManager.instance.currentDifficulty switch
+        {
+            EnemyDifficultyType.EasyMode => backgrounds[0],
+            EnemyDifficultyType.MediumMode => backgrounds[1],
+            EnemyDifficultyType.HardMode => backgrounds[2],
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        background.sprite = _selectedBackground;
+    }
+}
