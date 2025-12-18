5677198d11fc2d7fcec30e4aa478a8071144503c
Blechman Shy (NorthStar) <rakesh1985@gmail.com>
2025-12-18 22:18:39 +0200

    Leaves FX

--- Assets/Scripts/UI/BackgroundSwitch.cs
+++ Assets/Scripts/UI/BackgroundSwitch.cs
@@ -10,17 +10,38 @@ namespace SamuraiStandoff
         [SerializeField] private List<Sprite> backgrounds;
         [SerializeField] private Image background;
         private Sprite _selectedBackground;
+        [SerializeField] private GameObject GreenFX;
+        [SerializeField] private GameObject PinkFX;
 
         private void Awake()
         {
-            _selectedBackground = Resources.Load<GameData>("Game Data").currentDifficulty switch
+            var difficulty = Resources.Load<GameData>("Game Data").currentDifficulty;
+            
+            _selectedBackground = difficulty switch
             {
                 EnemyDifficultyType.EasyMode => backgrounds[0],
                 EnemyDifficultyType.MediumMode => backgrounds[1],
                 EnemyDifficultyType.HardMode => backgrounds[2],
                 _ => throw new ArgumentOutOfRangeException()
             };
+            
+            switch (difficulty)
+            {
+                case EnemyDifficultyType.EasyMode:
+                    GreenFX.SetActive(true);
+                    PinkFX.SetActive(false);
+                    break;
 
+                case EnemyDifficultyType.MediumMode:
+                case EnemyDifficultyType.HardMode:
+                    GreenFX.SetActive(false);
+                    PinkFX.SetActive(true);
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            
             background.sprite = _selectedBackground;
 
             AudioManager.instance.PlaySound("Waterfall");
