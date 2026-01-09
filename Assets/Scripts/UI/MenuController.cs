5c98ed5142579c2a414ef3c25283abb8105241a5
Blechman Shy (NorthStar) <rakesh1985@gmail.com>
2026-01-09 21:05:03 +0200

    Update MenuController.cs
    Initialization safeguard

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -248,6 +248,12 @@ namespace SamuraiStandoff
                     characterImage.sprite = playerSelectedCharacter.sprites[0];
                     selectedCharacterNameText.text = playerSelectedCharacter.name;
                 }
+                else
+                {
+                    playerData.selectedCharacter = gameData.allCharacters[0];
+                    characterImage.sprite = gameData.allCharacters[0].sprites[0];
+                    selectedCharacterNameText.text = playerSelectedCharacter.name;
+                }
             }
         }
 
