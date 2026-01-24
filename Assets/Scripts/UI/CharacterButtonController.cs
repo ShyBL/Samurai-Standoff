3bf8f7b064850d113d99101281659587c3d34bbd
Blechman Shy (NorthStar) <rakesh1985@gmail.com>
2026-01-24 19:40:24 +0200

    Lore hover effect

--- Assets/Scripts/UI/CharacterButtonController.cs
+++ Assets/Scripts/UI/CharacterButtonController.cs
@@ -1,15 +1,25 @@
 using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace SamuraiStandoff
 {
-    public class CharacterButtonController : MonoBehaviour
+    public class CharacterButtonController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         public CharacterType characterType;
         [SerializeField] private GameObject lockOverlay;
         [SerializeField] private GameObject characterPortrait;
+        [SerializeField] private GameObject hoverObject;
 
+        private void Awake()
+        {
+            // Ensure the hover object starts inactive
+            if (hoverObject != null)
+            {
+                hoverObject.SetActive(false);
+            }
+        }
         private void Start()
         {
             var unlocked = GameManager.instance.IsCharacterUnlocked(characterType);
@@ -17,7 +27,23 @@ namespace SamuraiStandoff
             button.interactable = unlocked;
             SetLockedVisual(!unlocked);
         }
-
+        
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (hoverObject != null)
+            {
+                hoverObject.SetActive(true);
+            }
+        }
+    
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (hoverObject != null)
+            {
+                hoverObject.SetActive(false);
+            }
+        }
+        
         private void SetLockedVisual(bool isLocked)
         {
             if (lockOverlay != null)
